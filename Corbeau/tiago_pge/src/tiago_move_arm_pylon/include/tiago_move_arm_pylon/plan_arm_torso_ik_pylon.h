1ae3101e0d516e1633d7ba3970a73ae7c8d8e108
AntoineVi <34862129+AntoineVi@users.noreply.github.com>
2020-02-08 16:53:42 +0100

    Boite englobante pour l'OT de TIAGo

--- Corbeau/tiago_pge/src/tiago_move_arm_pylon/include/tiago_move_arm_pylon/plan_arm_torso_ik_pylon.h
+++ Corbeau/tiago_pge/src/tiago_move_arm_pylon/include/tiago_move_arm_pylon/plan_arm_torso_ik_pylon.h
@@ -5,6 +5,7 @@
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <tf/transform_listener.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
 
 // Std C++ headers
 #include <string>
@@ -19,7 +20,8 @@ namespace tiago_move_arm_pylon
     {
     public:
         void go_to_point_arm_tool_link(geometry_msgs::PoseStamped goal_pose_pylon_frame);
-        float degree_to_radian(float degree);        
+        float degree_to_radian(float degree);
+        void add_aptere(); 
     };
 
 }
--- Corbeau/tiago_pge/src/tiago_move_arm_pylon/src/plan_arm_torso_ik_pylon.cpp
+++ Corbeau/tiago_pge/src/tiago_move_arm_pylon/src/plan_arm_torso_ik_pylon.cpp
@@ -37,6 +37,10 @@ void Move_tiago_arm::go_to_point_arm_tool_link(geometry_msgs::PoseStamped goal_p
 
   //select group of joints
   moveit::planning_interface::MoveGroupInterface group_arm_torso("arm_torso");
+
+  // Attach system aptere box to the TIAGo EE
+  add_aptere();
+
   //choose your preferred planner
   group_arm_torso.setPlannerId("SBLkConfigDefault");
   group_arm_torso.setPoseReferenceFrame("base_footprint");
@@ -73,6 +77,45 @@ void Move_tiago_arm::go_to_point_arm_tool_link(geometry_msgs::PoseStamped goal_p
   spinner.stop();
 }
 
+void Move_tiago_arm::add_aptere(){
+  moveit_msgs::AttachedCollisionObject attached_object;
+  attached_object.link_name = "gripper_grasping_frame";
+  /* The header must contain a valid TF frame*/
+  attached_object.object.header.frame_id = "gripper_grasping_frame";
+  /* The id of the object */
+  attached_object.object.id = "sys_aptere_box";
+  attached_object.object.header.stamp = ros::Time::now();
+
+  /* A default pose */
+  geometry_msgs::Pose pose;
+  pose.orientation.w = 1.0;
+
+  /* Define a box to be attached */
+  shape_msgs::SolidPrimitive primitive;
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[0] = 0.19;
+  primitive.dimensions[1] = 0.19;
+  primitive.dimensions[2] = 0.19;
+
+  attached_object.object.primitives.push_back(primitive);
+  attached_object.object.primitive_poses.push_back(pose);
+  attached_object.object.operation = attached_object.object.ADD;
+
+  // Links touched by the system aptere box
+  attached_object.touch_links = std::vector<std::string>{ "gripper_grasping_frame", "gripper_left_finger_link", "gripper_right_finger_link", "gripper_link", "gripper_tool_link" };
+
+  ros::NodeHandle nh("~");
+  ros::Publisher publisher = nh.advertise<moveit_msgs::AttachedCollisionObject>("/attached_collision_object", 1);
+    while(publisher.getNumSubscribers() < 1)
+    {
+      ROS_INFO_STREAM("Waiting for /attached_collision_object topic");
+      ros::WallDuration sleep_t(0.5);
+      sleep_t.sleep();
+    }
+    publisher.publish(attached_object);
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "plan_arm_torso_ik_pylon");
@@ -101,5 +144,4 @@ int main(int argc, char** argv)
 	tiago_arm.go_to_point_arm_tool_link(goal_pose_pylon_frame);
 
   return EXIT_SUCCESS;
-
 }
